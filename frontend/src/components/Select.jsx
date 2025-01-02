20a608bbbfbe0a40e7b3f307bb3ca09a5f9b0d19
furkanerdogan <furkanerdd1@gmail.com>
2025-01-02 14:21:37 +0300

    fixed errors

--- frontend/src/components/Select.jsx
+++ frontend/src/components/Select.jsx
@@ -5,8 +5,8 @@ function Select({text,name,value,onChange,option1,option2}) {
     <>
     <div className='mb-3'>
       <label className="form-label fw-bold">{text}</label>
-         <select class="form-select" id="floatingSelect" name={name} value={value} onChange={onChange} >
-                <option selected>{`Select the ${text}`}</option>
+         <select className="form-select" id="floatingSelect" name={name} value={value} onChange={onChange} >
+                <option>{`Select the ${text}`}</option>
                 <option value={option1}>{option1}</option>
                 <option value={option2}>{option2}</option>
         </select>
