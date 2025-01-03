b4ef4fd3f18db4f4f024962587a0186b53b878a2
furkanerdogan <furkanerdd1@gmail.com>
2025-01-03 18:11:30 +0300

    refactor:fixed multiple structural issues

--- backend/src/main/java/com/furkanerd/backend/controller/CourseController.java
+++ backend/src/main/java/com/furkanerd/backend/controller/CourseController.java
@@ -1,7 +1,10 @@
 package com.furkanerd.backend.controller;
 
 import com.furkanerd.backend.model.entity.Course;
-import com.furkanerd.backend.repository.CourseRepository;
+import com.furkanerd.backend.service.CourseService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,32 +16,31 @@ import java.util.Optional;
 @RequestMapping("/api/courses")
 public class CourseController {
 
-    private final CourseRepository courseRepository;
+    private final CourseService courseService;
 
-    public CourseController(CourseRepository courseRepository){
-        this.courseRepository=courseRepository;
+    public CourseController(CourseService courseService){
+        this.courseService=courseService;
     }
 
     @GetMapping
-    public ResponseEntity<List<Course>> getAllCourses(){
-        return ResponseEntity.ok(courseRepository.findAll());
+    public ResponseEntity<Page<Course>> getAllUsers(@RequestParam(defaultValue = "0") Integer page,
+                                                  @RequestParam(defaultValue = "10") Integer pageSize) {
+        return ResponseEntity.ok(courseService.getAll(PageRequest.of(page,pageSize, Sort.by("id"))));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Course>> getCourseById(@PathVariable Integer id) {
-        return ResponseEntity.ok(courseRepository.findById(id));
+    public ResponseEntity<Course> getCourseById(@PathVariable Integer id) {
+        return ResponseEntity.ok(courseService.findById(id));
     }
 
     @PostMapping
     public ResponseEntity<Course> createCourse(@RequestBody Course course){
-        return ResponseEntity.ok(courseRepository.save(course));
+        return ResponseEntity.ok(courseService.save(course));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCourse(@PathVariable Integer id){
-        courseRepository.deleteById(id);
+        courseService.delete(id);
         return ResponseEntity.ok().build();
     }
-
-
 }
